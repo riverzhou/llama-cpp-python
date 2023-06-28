6427f8572ac33585c1114ea946a9a2d7846389f6
River Zhou <riverzhou2000@163.com>
2023-06-28 22:30:49 +0800

    Function: Script for save log from redis

---
+++ log/llama.log.py
@@ -0,0 +1,26 @@
+#!/bin/env python3
+
+import redis
+import json
+import os
+from datetime import datetime
+
+logDir  = '/var/log/llama.cpp'
+logJsonName = 'llama.log'
+logJsonFile = os.path.join(logDir, logJsonName)
+
+if not os.path.exists(logDir):
+    os.makedirs(logDir)
+
+rediscon = redis.StrictRedis(host='127.0.0.1', port=6379, db=0)
+print(rediscon)
+
+with open(logJsonFile, 'a') as fJson:
+    while True:
+        logDataStr = rediscon.blpop('llama.cpp')[1].decode('utf8')
+        ts = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
+        logDict = {'time':ts, 'log':json.loads(logDataStr)}
+        logJsonStr = json.dumps(logDict, indent=4, ensure_ascii=False)
+        fJson.write(logJsonStr + '\n')
+        fJson.flush()
+
